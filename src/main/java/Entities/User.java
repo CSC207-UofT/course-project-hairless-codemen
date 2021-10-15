059555270be5dcc3c4d8715e0cbf5157ea8a901d
howardxhc <howard.xiao@mail.utoronto.ca>
2021-10-15 16:51:33 -0400

    Modify User

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -15,6 +15,7 @@ public class User {
         this.password = password;
         this.cart = new Cart();
     }
+
     /**
      * Create another constructor for invited customers, they have some money after they create account.
      */
@@ -25,52 +26,49 @@ public class User {
         this.password = password;
         this.cart = new Cart();
     }
+
     /**
      * Change the User's name by using changeName
      * Return nothing
      */
     public void changeName(String new_name){
-
         this.name = new_name;
     }
+
     /**
      * Change the current password by using setPassword
      * Return nothing
      */
     public void setPassword(String new_password){
-
         this.password = new_password;
     }
+
     /**
      * Check the User's current name
      * Return a string representing the name of the User
      */
     public String getName(){
-
         return this.name;
     }
+
     /**
      * Check the amount of the money in the wallet
      * Return a type double representing the money
      */
     public Wallet getWallet(){
-
         return this.wallet;
     }
 
     @Override
     public String toString() {
-
         return getName();
     }
 
     public Cart getCart(){
-
         return this.cart;
     }
 
     public int getId(){
-
         return this.id;
     }
 

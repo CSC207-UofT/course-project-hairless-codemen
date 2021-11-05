f693cdac7afad1835b335d43af2d65048d10595a
InxiQ <xiao.qin@mail.utoronto.ca>
2021-11-04 22:03:42 -0400

    Added try catch UI

--- src/main/java/UI/Entry.java
+++ src/main/java/UI/Entry.java
@@ -1,9 +1,12 @@
 package src.main.java.UI;
 import src.main.java.Controller.Login;
+import src.main.java.Entities.User;
+import src.main.java.Entities.UserStorage;
 import src.main.java.Use_cases.UserReadWriter;
 
 
 import javax.swing.*;
+import java.io.EOFException;
 import java.io.IOException;
 
 import static src.main.java.Controller.Login.logout;
@@ -15,6 +18,14 @@ public class Entry {
         Login login = new Login();
         Frame frame = new Frame(login);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try{
+            UserReadWriter.readFromFile();
+        }catch (EOFException e){
+            User u=new User("default", "000000");
+            UserStorage.addElement(u);
+            UserReadWriter.SaveIntoFile(UserStorage.getUserList());
+            UserReadWriter.readFromFile();
+        }
         UserReadWriter.readFromFile();
 
         frame.addWindowFocusListener(new java.awt.event.WindowAdapter() {

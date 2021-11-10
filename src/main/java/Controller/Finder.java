99000b2657e1bef0abee188b81ac38429ecd35ce
liuqin26 <qingyi.liu@mail.utoronto.ca>
2021-11-09 19:01:02 -0500

    Finder.java

--- src/main/java/Controller/Finder.java
+++ src/main/java/Controller/Finder.java
@@ -14,20 +14,31 @@ public class Finder {
     public static ArrayList<Item> find(String target){
         String user = "";
         for (String key: UserManager.getUserList().keySet()){
-            if (target.equals(key)){user = key;}
+            if (target.equals(key)){
+                user = key;
+            }
         }
-        if (user.equals("")){return ItemManager.search(target);}
+        if (user.equals("")){
+            return ItemManager.search(target);
+        }
         return ItemManager.search_by_owner(user);
     }
 
     public static ArrayList<Item> find(String target, Find_By f){
-        if (f.equals(Find_By.NAME)){return ItemManager.search(target);}
-        else if (f.equals(Find_By.OWNER)){return ItemManager.search_by_owner(target);}
-        else if (f.equals(Find_By.CATEGORY)){return ItemManager.search_by_category(target);}
+        if (f.equals(Find_By.NAME)){
+            return ItemManager.search(target);
+        }
+        else if (f.equals(Find_By.OWNER)){
+            return ItemManager.search_by_owner(target);
+        }
+        else if (f.equals(Find_By.CATEGORY)){
+            return ItemManager.search_by_category(target);
+        }
         return null;
     }
 
     public static Order find(int target){
+
         return (Order) OrderManager.search(target);
     }
 

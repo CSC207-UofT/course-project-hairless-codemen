e0340d712af6de7e034fd77e95d029a23292586c
liuqin26 <qingyi.liu@mail.utoronto.ca>
2021-11-14 01:27:44 -0500

    Fixing bugs in FinderTest.java

--- src/test/java/Controller/FinderTest.java
+++ src/test/java/Controller/FinderTest.java
@@ -33,22 +33,22 @@ public class FinderTest {
         item1 = new Item("Banana", seller, 40, "Fruit");
         item2 = new Item("Apple", seller, 60, "Fruit");
         order = new Order(123, lst, buyer, seller, 100);
-        lst.add(item2);
-        lst.add(item1);
-        storages.put(123, order);
-        OrderStorage.addElement(storages);
-        ItemStorage.addElement(lst);
+
     }
 
     @Test
     public void TestFinderOrder1() throws IOException, ClassNotFoundException{
 
+        storages.put(123, order);
+        OrderStorage.addElement(storages);
         assertNull(Finder.find(601));
     }
 
     @Test
     public void TestFinderOrder2() throws IOException, ClassNotFoundException{
 
+        storages.put(123, order);
+        OrderStorage.addElement(storages);
         assertTrue(OrderManager.has_order(123));
         assertEquals(Finder.find(123), order);
 
@@ -57,16 +57,19 @@ public class FinderTest {
     @Test
     public void TestFinderName1() throws IOException, ClassNotFoundException{
 
-
+        lst.add(item2);
+        lst.add(item1);
+        ItemStorage.addElement(lst);
         assertEquals(Finder.find("Orange", Finder.Find_By.NAME), empty);
     }
 
     @Test
     public void TestFinderName2() throws IOException, ClassNotFoundException{
 
-        ArrayList<Item> lst_1 = new ArrayList<>();
-        lst_1.add(item2);
-        assertEquals(Finder.find("Apple", Finder.Find_By.NAME), lst_1);
+        assertEquals(Objects.requireNonNull(Finder.find("Apple", Finder.Find_By.NAME)).size(), 1);
+        assertEquals(Objects.requireNonNull(Finder.find("Apple", Finder.Find_By.NAME)).get(0).getItemName(),
+                "Apple");
+
     }
 
     @Test
@@ -79,16 +82,23 @@ public class FinderTest {
     @Test
     public void TestFinderOwner2() throws IOException, ClassNotFoundException{
 
-        assertEquals(Finder.find("happysell", Finder.Find_By.OWNER), lst);
+        assertEquals(Objects.requireNonNull(Finder.find("happysell", Finder.Find_By.OWNER)).size(), 2);
+        assertEquals(Objects.requireNonNull(Finder.find("happysell", Finder.Find_By.OWNER)).get(0).getItemName(),
+                "Apple");
+        assertEquals(Objects.requireNonNull(Finder.find("happysell", Finder.Find_By.OWNER)).get(1).getItemName(),
+                "Banana");
     }
 
     @Test
     public void TestFinderCategory1() throws IOException, ClassNotFoundException{
+
+
         assertEquals(Finder.find("Dessert", Finder.Find_By.CATEGORY), empty);
     }
 
     @Test
     public void TestFinderCategory2() throws IOException, ClassNotFoundException{
+
 
         assertEquals(Finder.find("Fruit", Finder.Find_By.CATEGORY), lst);
     }

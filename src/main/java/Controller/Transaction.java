432215dabd2cb27a6b8431c87e9259862bef609e
zhuhongd <zhuhongda2000@gmail.com>
2021-12-04 16:41:24 -0500

    10/31/2021 Hongda' change on transactions and Managers

--- src/main/java/Controller/Transaction.java
+++ src/main/java/Controller/Transaction.java
@@ -54,6 +54,7 @@ public class Transaction {
 
             UserManager.subtractMoney(buyer, ItemManager.get_all_price(items));   //subtract buyer money
             CartManager.remove_items(buyer, items);                   //remove the items in buyer's cart
+            ItemManager.removeElement(items);                         // remove the item from the itemstorage
 
             return true;
         }
